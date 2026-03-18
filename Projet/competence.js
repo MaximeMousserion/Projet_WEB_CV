66539cc958dc9f2503cb0f062d1c908f4758ed89
MaximeMousserion <max.mousserion@gmail.com>
2026-03-18 10:00:11 +0100

    Delete Projet directory

--- Projet/competence.js
+++
@@ -1,9 +0,0 @@
-function toggle(element) {
-    let list = element.nextElementSibling;
-
-    if (list.classList.contains("hidden")) {
-        list.classList.remove("hidden");
-    } else {
-        list.classList.add("hidden");
-    }
-}
